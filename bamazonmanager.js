d25442d778d64a92466f824df6d6eb2183f53d8f
Gerald Porter <porter.glp2010@gmail.com>
2017-11-20 13:37:04 -0500

    mad some updates

--- bamazonmanager.js
+++ bamazonmanager.js
@@ -11,13 +11,12 @@
 //	If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" 
 //       of any item currently in the store.
 //	If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
-________________________________________
-
-
+// Load the NPM Package inquirervar productId = 0;
 
 var qtyToPurchase = 5;
 var qtyInStock = 0;
 var quitFlag = false;
+var newStockQty = 0;
 
 
 var inquirer = require("inquirer");
@@ -126,15 +125,64 @@ function displayProducts() {
         connection.end();
 
     })
-}
+} //end displayProducts
+
+
+function displayLowInventory() {
+
+    connection.query('SELECT * FROM ProductsTable WHERE stock_quantity < 60', function(err, res) {
+        if (err) throw err;
+
+        var table = new Table({
+            head: ['ITEM ID', 'PRODUCT', 'DEPARTMENT', 'PRICE', 'STOCK']
+
+        })
+
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+        }
+
+
+        console.log(table.toString());
+
+        connection.end();
+
+    })
+} //end function displayLowInventory()
+
+
+function addToInventory(anItem_id, aQuantity) {
+    // get current stock_quantity
+    var sql = "SELECT * FROM ProductsTable WHERE item_id = " + anItem_id;
+    console.log("SQL: " + sql);
+    connection.query(sql, function(err, result) {
+        if (err) {
+            throw err;
+            console.log(result.affectedRows + " record(s) updated");
+        } else {
+            newStockQty = parseInt(result[0].stock_quantity) + aQuantity;
+            console.log("New Stock Quantity: " + newStockQty);
+
+            var sql = "UPDATE ProductsTable SET stock_quantity = " + newStockQty + " WHERE item_id = " + anItem_id;
+            console.log("SQL: " + sql);
+            connection.query(sql, function(err, result) {
+                if (err) {
+                    throw err;
+                } else { //qtyInStock > qtyToPurchase
+                    console.log(result.affectedRows + " record(s) updated");
+                }
+
+            });
+
+        }
+    });
+} //end function addToInventory()
 
 
 
 
-
-
-
-
+// Load the NPM Package inquirer
+var inquirer = require("inquirer");
 
 // ========================================================================
 // Load the NPM Package inquirer
@@ -151,25 +199,61 @@ inquirer.prompt([
     }
 
 ]).then(function(inquirerResponse) {
-
+    console.log(inquirerResponse);
     // If manager wishes to View Products for Sale
-    if (inquirer.inquirerResponse === "View Products for Sale") {
+    if (inquirerResponse.options === "View Products for Sale") {
         //Execute View Products for Sale logic    
-        displayProducts()
+        displayProducts();
     }
     // If manager wishes to View Low Inventory
     else
-    if (inquirer.inquirerResponse === "View Low Inventory") {
+    if (inquirerResponse.options === "View Low Inventory") {
         //Execute View Log Inventory
+        displayLowInventory();
     }
     // If manager wishes to Add to Inventory
     else
-    if (inquirer.inquirerResponse === "Add to Inventory") {
+    if (inquirerResponse.options === "Add to Inventory") {
         //Execute Add to Inventory Logic
+        // Create a "Prompt" with a series of questions.
+        inquirer
+            .prompt([
+                // Prompt user for id of product to purchase
+                {
+                    type: "input",
+                    message: "Product ID ",
+                    name: "productId"
+                },
+
+                // Prompt user for quantity of product to purchase
+                {
+                    type: "input",
+                    message: "Quantity to Add?",
+                    name: "productQty"
+                },
+                // Here we ask the user to confirm.
+                {
+                    type: "confirm",
+                    message: "Are you sure:",
+                    name: "confirm",
+                    default: true
+                }
+            ])
+            .then(function(inquirerResponse) {
+                // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+                if (inquirerResponse.confirm) {
+                    console.log("\nProduct Id entered: " + inquirerResponse.productId);
+                    console.log("\nProduct Quantity to Add: " + inquirerResponse.productQty);
+                    addToInventory(inquirerResponse.productId, parseInt(inquirerResponse.productQty));
+                    displayProducts();
+                } else {
+                    console.log("\nThat's okay, come again when you are more sure.\n");
+                }
+            });
     }
     // If manager wishes to Add New Product
     else
-    if (inquirer.inquirerResponse === "Add to Product") {
+    if (inquirerResponse.options === "Add to Product") {
         //Execute View Log Inventory
     }
 });
